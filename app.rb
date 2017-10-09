31c20c1b99c6b630ef67d050077268c9e57be516
vadimgavrish <vadim@gavrish.ca>
2017-10-09 15:54:18 -0400

    added view all and specific person

--- app.rb
+++ app.rb
@@ -6,10 +6,35 @@ def readJson
   @people = @customers['people']
 end
 
+def allPeople
+    j = 0
+    while j < @people.length
+        date = Date.parse(@people[j]['dateOfBirth'])
+        @people[j]['dateOfBirth'] = date.strftime('%B %-d %Y')
+    j += 1
+    end
+end
+
+def specificPerson
+    i = 0
+    while i < @people.length
+        if @people[i]['id'] == params["id"]
+            @person = @people[i]
+        end
+        i += 1
+    end
+end
+
 get '/' do
+  readJson
+  allPeople
+
   erb :index
 end
 
 get '/id' do
+  readJson
+  specificPerson
+
   erb :person
 end
