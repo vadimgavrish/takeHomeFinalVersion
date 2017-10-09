7c17a5d9a5586b3feb3de10694de9d8f5bf91b9d
vadimgavrish <vadim@gavrish.ca>
2017-10-09 17:47:25 -0400

    finalized

--- app.rb
+++ app.rb
@@ -25,6 +25,39 @@ def specificPerson
     end
 end
 
+def bestMatches
+    most_common = 0
+    @best_matches = []
+    z = 0
+    while z < @people.length
+        if @person['id'] != @people[z]['id']
+            same = @person['hobbies'] & @people[z]['hobbies']
+            if same.length > most_common
+                most_common = same.length
+                most_common_friend = @people[z]['name']
+            end
+        end
+    z += 1
+    end
+
+    second_most_common = 0
+    x = 0
+    while x < @people.length
+        if @person['name'] != @people[x]['name']
+            if @people[x]['name'] != most_common_friend
+                same_hobbies = @person['hobbies'] & @people[x]['hobbies']
+                if same.length > second_most_common
+                    second_most_common = same.length
+                    second_most_common_friend = @people[x]['name']
+                end
+            end
+        end
+    x += 1
+    end
+
+    @best_matches.push(most_common_friend, second_most_common_friend)
+end
+
 get '/' do
   readJson
   allPeople
@@ -32,9 +65,10 @@ get '/' do
   erb :index
 end
 
-get '/id' do
+get '/:id' do
   readJson
   specificPerson
+  bestMatches
 
   erb :person
 end
